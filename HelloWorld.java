e559d10862578e8b33bcfa9f362cfbec01cc3f03
unknown <binhnguyen2572005@gmail.com>
2025-03-01 15:19:52 -0800

    Assigment 2.2.4

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String args[]) {
+        System.out.println("Xin chào \n các bạn!");  // Xuống dòng
+        System.out.println("Hello \t world!");      // Thêm khoảng trắng (tab)
+    }
+}
